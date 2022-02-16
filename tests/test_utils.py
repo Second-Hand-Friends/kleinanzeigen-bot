12ff0bb3c3a88c9c882b0cd9fc10b238d4e90f8c
sebthom <sebthom@users.noreply.github.com>
2022-02-16 21:55:57 +0100

    extend utils.ensure()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,31 @@ Copyright (C) 2022 Sebastian Thomschke and contributors
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
 import os, sys, time
+import pytest
 from kleinanzeigen_bot import utils
+
+
+def test_ensure():
+    utils.ensure(True, "TRUE")
+    utils.ensure("Some Value", "TRUE")
+    utils.ensure(123, "TRUE")
+    utils.ensure(-123, "TRUE")
+    utils.ensure(lambda: True, "TRUE")
+
+    with pytest.raises(AssertionError):
+        utils.ensure(False, "FALSE")
+
+    with pytest.raises(AssertionError):
+        utils.ensure(0, "FALSE")
+
+    with pytest.raises(AssertionError):
+        utils.ensure("", "FALSE")
+
+    with pytest.raises(AssertionError):
+        utils.ensure(None, "FALSE")
+
+    with pytest.raises(AssertionError):
+        utils.ensure(lambda: False, "FALSE", timeout = 2)
 
 
 def test_pause():
@@ -13,4 +37,4 @@ def test_pause():
     if sys.platform == "darwin" and os.getenv("GITHUB_ACTIONS", "true") == "true":
         assert 99 < elapsed < 300
     else:
-        assert 99 < elapsed < 110
+        assert 99 < elapsed < 120
