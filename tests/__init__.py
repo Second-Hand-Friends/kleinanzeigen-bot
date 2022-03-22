6aaf2076d8bdb8c7991a6a3f58430ea89e0b9415
sebthom <sebthom@users.noreply.github.com>
2022-03-22 14:12:52 +0100

    upgrade webdriver-manager and improve browser version detection

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,12 @@
 Copyright (C) 2022 Sebastian Thomschke and contributors
 SPDX-License-Identifier: AGPL-3.0-or-later
 """
+import logging
+from typing import Final
+
+from kleinanzeigen_bot import utils
+
+utils.configure_console_logging()
+
+LOG:Final[logging.Logger] = logging.getLogger("kleinanzeigen_bot")
+LOG.setLevel(logging.DEBUG)
