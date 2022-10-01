120e53628eaa513546fb67db10cffebf10901f4f
Philipp K <philippkubon@protonmail.com>
2022-10-01 13:05:30 +0200

    Extract ad details

--- kleinanzeigen_bot/__init__.py
+++ kleinanzeigen_bot/__init__.py
@@ -750,9 +750,17 @@ class KleinanzeigenBot(SeleniumMixin):
         info['description'] = descr
 
         # get special attributes
-        # TODO get attributes
         details_box = self.webdriver.find_element(By.CSS_SELECTOR, '#viewad-details')
         assert details_box
+        details_list = details_box.find_element(By.XPATH, './/ul')
+        assert details_list
+        list_items = details_list.find_elements(By.TAG_NAME, 'li')
+        details = dict()
+        for list_item in list_items:
+            detail_key = list_item.text.split('\n')[0]
+            detail_value = list_item.find_element(By.TAG_NAME, 'span').text
+            details[detail_key] = detail_value
+        info['special_attributes'] = details
 
         # process pricing
         try:
