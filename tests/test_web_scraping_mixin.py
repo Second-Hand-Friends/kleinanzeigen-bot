a441c5de7315c300741748b551abbf4877a554f4
sebthom <sebthom@users.noreply.github.com>
2024-03-07 20:26:40 +0100

    replace selenium with nodriver

---
+++ tests/test_web_scraping_mixin.py
@@ -0,0 +1,41 @@
+"""
+SPDX-FileCopyrightText: © Sebastian Thomschke and contributors
+SPDX-License-Identifier: AGPL-3.0-or-later
+SPDX-ArtifactOfProjectHomePage: https://github.com/Second-Hand-Friends/kleinanzeigen-bot/
+"""
+import logging, os, time
+from typing import Any
+
+import nodriver, pytest
+from flaky import flaky
+
+from kleinanzeigen_bot.web_scraping_mixin import WebScrapingMixin
+from kleinanzeigen_bot.utils import ensure
+
+if os.environ.get("CI"):
+    logging.getLogger("kleinanzeigen_bot").setLevel(logging.DEBUG)
+    logging.getLogger("nodriver").setLevel(logging.DEBUG)
+
+
+def delay_rerun(*args:Any) -> bool:  # pylint: disable=unused-argument
+    time.sleep(5)
+    return True
+
+
+async def atest_init() -> None:
+    web_scraping_mixin = WebScrapingMixin()
+
+    browser_path = web_scraping_mixin.get_compatible_browser()
+    ensure(browser_path is not None, "Browser not auto-detected")
+
+    web_scraping_mixin.close_browser_session()
+    try:
+        await web_scraping_mixin.create_browser_session()
+    finally:
+        web_scraping_mixin.close_browser_session()
+
+
+@flaky(max_runs = 3, min_passes = 1, rerun_filter = delay_rerun)  # type: ignore[misc] # mypy
+@pytest.mark.itest
+def test_init() -> None:
+    nodriver.loop().run_until_complete(atest_init())
