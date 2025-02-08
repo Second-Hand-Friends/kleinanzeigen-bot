1e880749e9d901f9074225251165a24c0ab6b156
Jens Bergmann <1742418+1cu@users.noreply.github.com>
2025-02-08 22:52:38 +0100

    Corrected copyright information
    The copyright information in the following files has been updated to reflect the correct author:
    
    - src/kleinanzeigen_bot/logging.py
    - tests/unit/test_logging.py

--- src/kleinanzeigen_bot/logging.py
+++ src/kleinanzeigen_bot/logging.py
@@ -1,5 +1,5 @@
 """
-SPDX-FileCopyrightText: © Sebastian Thomschke and contributors
+SPDX-FileCopyrightText: © Jens Bergmann and contributors
 SPDX-License-Identifier: AGPL-3.0-or-later
 SPDX-ArtifactOfProjectHomePage: https://github.com/Second-Hand-Friends/kleinanzeigen-bot/
 """
