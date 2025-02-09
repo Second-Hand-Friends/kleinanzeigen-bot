586da290f6bb92dc57c653678e52ee508abfa029
Jens Bergmann <1742418+1cu@users.noreply.github.com>
2025-02-09 01:22:04 +0100

    enh: add author and license

--- tests/unit/test_prefix_suffix.py
+++ tests/unit/test_prefix_suffix.py
@@ -1,3 +1,8 @@
+"""
+SPDX-FileCopyrightText: © Jens Bergmann and contributors
+SPDX-License-Identifier: AGPL-3.0-or-later
+SPDX-ArtifactOfProjectHomePage: https://github.com/Second-Hand-Friends/kleinanzeigen-bot/
+"""
 from typing import Any
 import pytest
 from kleinanzeigen_bot import KleinanzeigenBot
