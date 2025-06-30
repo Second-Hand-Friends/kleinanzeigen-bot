66ac61f859eeee4eded2e249bf027a5a6189281a
Jens Bergmann <1742418+1cu@users.noreply.github.com>
2025-06-30 08:37:45 +0200

    fix: Removed unnecessary hashes in docstring

--- src/kleinanzeigen_bot/utils/files.py
+++ src/kleinanzeigen_bot/utils/files.py
@@ -6,9 +6,9 @@ import os
 
 def abspath(relative_path:str, relative_to:str | None = None) -> str:
     """Return an absolute path based on *relative_to*.
-    # This function ensures that the returned path is always absolute, regardless of whether
-    # the input 'relative_to' is absolute or relative. This is achieved by normalizing
-    # 'relative_to' to an absolute path before joining it with 'relative_path'.
+    This function ensures that the returned path is always absolute, regardless of whether
+    the input 'relative_to' is absolute or relative. This is achieved by normalizing
+    'relative_to' to an absolute path before joining it with 'relative_path'.
     """
 
     if not relative_to:
