80b3400541001aebb34e2bb83dd94a2385211954
Jens <1742418+1cu@users.noreply.github.com>
2025-11-14 13:51:43 +0100

    test: add more unit test markers and formatting

--- tests/unit/test_price_reduction.py
+++ tests/unit/test_price_reduction.py
@@ -21,7 +21,7 @@ class _ApplyAutoPriceReduction(Protocol):
 
 @pytest.fixture
 def apply_auto_price_reduction() -> _ApplyAutoPriceReduction:
-    bot: Any = KleinanzeigenBot()
+    bot:Any = KleinanzeigenBot()
     return cast(_ApplyAutoPriceReduction, bot._KleinanzeigenBot__apply_auto_price_reduction)
 
 
