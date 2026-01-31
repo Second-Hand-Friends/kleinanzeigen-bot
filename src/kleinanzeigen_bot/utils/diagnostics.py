ba9cbc81a0909cb90cc5153220961af32b227ad3
Jens <1742418+1cu@users.noreply.github.com>
2026-01-31 20:38:05 +0100

    Update copyright information in diagnostics.py

--- src/kleinanzeigen_bot/utils/diagnostics.py
+++ src/kleinanzeigen_bot/utils/diagnostics.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2025 Sebastian Thomschke and contributors
+# SPDX-FileCopyrightText: © Jens Bergmann and contributors
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # SPDX-ArtifactOfProjectHomePage: https://github.com/Second-Hand-Friends/kleinanzeigen-bot/
 import asyncio, json, re, secrets, shutil  # isort: skip
